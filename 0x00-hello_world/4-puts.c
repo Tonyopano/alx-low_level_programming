a8558286aa5987bd2efcb7f8e5075c9228dab76d
Tonyopano <antonyopano@gmai.com>
2022-06-30 16:28:25 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-
-
+#include<stio.h>
 
 /**
+ * main - print the string in the out function
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Description: using the main fuction
+ * this program prints "programming is like building multilingual puzzle
+ * Return: 0
+ */
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
