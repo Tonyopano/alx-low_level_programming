e6843360bf80cd3729e67cdc9ad468b435390ed7
Tonyopano <antonyopano@gmai.com>
2022-06-30 15:50:25 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,52 +1,31 @@
-include <stdio.h>
+#include <stdio.h>
 
 
 
 /**
  *
- * * main - print the string in the put function
+ *  * main - Entry point
  *
- * *
+ *   *
  *
- * * Description: using the main function
+ *    * Return: Always 0 (Success)
  *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ *     */
 
 int main(void)
-	
+
 {
 
-	char c;
+		printf("Size of a char: %ld byte(s)\n", sizeof(char));
 
-	int i;
+			printf("Size of an int: %ld byte(s)\n", sizeof(int));
 
-	long li;
+				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 
-	long long lli;
+					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 
-	float f;
+						printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
+							return (0);
 
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-        printf("Size of a char: %ld byte(s)\n", sizeof(c));
-
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-        printf("Size of a float: %ld byte(s)\n", sizeof(f));
-
-	return (0);
 }
