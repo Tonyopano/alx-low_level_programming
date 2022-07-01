b7bab3a2a65b7c586050b755026a4e8cce9e772b
Tonyopano <antonyopano@gmai.com>
2022-07-01 14:22:48 -0700

    Advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+100 print                                                                                  /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int i, j;
+
+for (j = 48 ; j < 58 ; j++)
+{
+for (i = j + 1 ; i < 58 ; i++)
+{
+putchar(j);
+putchar(i);
+if (j != 56 || i != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
