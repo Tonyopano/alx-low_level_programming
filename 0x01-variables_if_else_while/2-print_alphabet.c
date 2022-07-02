6eb32a15179bd40af3e42a08318455e128910ef3
Tonyopano <antonyopano@gmai.com>
2022-07-02 03:18:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	printf("%c", letter);
+	printf("\n");
+	return (0);
+}
