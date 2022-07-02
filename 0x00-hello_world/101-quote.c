dccdc6355c6d7321796075071bb4f7fa5be4abcd
Tonyopano <antonyopano@gmai.com>
2022-07-02 03:46:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
-
 #include <string.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-			size_t len = strlen(str);
-
-				/* Starting main */
-
-			fwrite(str, len, 1, stderr);
-
-					return (1);
-
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = strlen(str);
+	/* Starting main */
+	fwrite(str, len, 1, stderr);
+	return (1);
 }
