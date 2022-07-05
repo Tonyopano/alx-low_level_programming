acad4a1309e0102a569f74f1ec25326a6b914694
Tonyopano <antonyopano@gmai.com>
2022-07-05 07:12:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,24 @@
-#include <main.h>
+include "main.h"
+
 
 /**
- * main - Desription of the nested look in the example below
+ * main - Entry point
  *
- * Return: Awalys 0.
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
-~
-~
-"0-putchar.c" 21L, 289C  
+
