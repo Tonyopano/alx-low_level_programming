f075b25e15dd4270e572b6bcb7332728e048531d
Tonyopano <antonyopano@gmai.com>
2022-07-05 06:09:43 -0700

    nested if task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef  MAIN_H
-#define  MAIN_H
-
 int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,3 +11,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif	/*  MAIN_H *‎
