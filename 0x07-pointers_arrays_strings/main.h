6eea697db0311603fd77e9f4693dbbce880e0152
Tonyopano <antonyopano@gmai.com>
2022-07-18 04:28:41 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
