c0db779ea9855ade81e503fa22b506de431c4994
Tonyopano <antonyopano@gmai.com>
2022-07-23 09:41:36 -0700

    program printing its self

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
 *Return: 0 (success)
 */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-	return (0)
+	return (0;)
 }
